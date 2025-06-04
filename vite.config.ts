dcb7dcc90301bc66723fc2fd1a150de34e99db50
Ada-Au <adaau0209@gmail.com>
2025-06-04 11:38:26 +0800

    update base path for gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -15,6 +15,6 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-  base: '/',
+  base: 'TB_Web/',
   // base: process.env.VITE_NODE_ENV === 'testing' ? 'TB_Web/' : '/',
 })
